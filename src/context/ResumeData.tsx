85bfe8070cb9f4a4694dd9058ac34c0bda19a415
HyoGeunGit <shimhg02@naver.com>
2021-07-05 10:18:28 +0900

    [ADD] Company

---
+++ src/context/ResumeData.tsx
@@ -0,0 +1,130 @@
+import sunrin from "../assets/School/sunrin.png";
+export const ResumeData = {
+  career: [
+    // 경력
+    {
+      company: "금성출판사 매쓰클라우드 팀",
+      startAt: "2020년 1월", // 입사 월
+      endAt: "",
+      job: "개발팀장", // 배정받은 일
+      description:
+        "개발팀에서의 매니지먼트와 소스 검수, PM역할을 하고있습니다. Kotlin을 사용한 안드로이드 개발과 Node.js 지원을 주로 하고 있습니다.",
+      icon: "https://firebasestorage.googleapis.com/v0/b/hyogeun-portfolio.appspot.com/o/company%2Fmath_logo.jpeg?alt=media&token=3757e86a-e81e-4215-abb6-040aa05e76a4",
+    },
+    {
+      company: "손유상 정보영재학원 대치점",
+      startAt: "2019년 10월",
+      endAt: "2020년 1월",
+      job: "Java, JS 강사",
+      description: "실전프로젝트반에서 Java를 사용한 안드로이드 개발과 node.js를 사용한 서버개발을 중/고등학생들에게 가르쳤습니다.",
+      icon: "https://firebasestorage.googleapis.com/v0/b/hyogeun-portfolio.appspot.com/o/company%2Fkoala.jpeg?alt=media&token=87446814-1157-47c1-b769-93144a1bee8c"
+    },
+    {
+      company: "언더핀 (Underpin)",
+      startAt: "2019년 2월",
+      endAt: "2019년 9월",
+      job: "안드로이드 개발자",
+      description: "RxKotlin을 사용한 안드로이드 개발을 맡았습니다.",
+      icon: "https://firebasestorage.googleapis.com/v0/b/hyogeun-portfolio.appspot.com/o/company%2Funderpin.jpeg?alt=media&token=e974a365-f487-4393-af8b-a6b646cb7498",
+    },
+    {
+      company: "SlabSoftWare",
+      startAt: "2018년 4월",
+      endAt: "2018년 12월",
+      job: "서버 개발자",
+      description: "Node.js를 사용한 서버 개발과 Delphi기반 클라이언트 개발을 맡았습니다.",
+      icon: "https://firebasestorage.googleapis.com/v0/b/hyogeun-portfolio.appspot.com/o/company%2Fslab.jpeg?alt=media&token=536fb746-8b2b-4927-bb15-8b5b66e826de"
+    },
+  ],
+  education: [
+    {
+      school: "선린인터넷고등학교",
+      startAt: "2018년",
+      endAt: "2021년",
+      department: "소프트웨어과", // 학과
+      icon: sunrin,
+    },
+  ],
+  awards: [
+    {
+      title: "Smarteen App Challenge 2020 엔터테인먼트 부문 대상",
+      host: "SK 플래닛", //주최
+      date: "2020년 10월", // 수상 일
+      description:
+        "Smarteen App+ Challenge 2020 엔터테인먼트 부문에서 한손으로 즐기는 하드코어 리듬게임 XStep을 개발하여 대상을 수상하였습니다. node.js를 사용한 서버개발, React를 사용한 웹 개발을 맡았습니다.", // 설명
+    },  
+    {
+      title: "Smarteen App Challenge 2019 생활부문 최우수상",
+      host: "SK 플래닛", //주최
+      date: "2019년 11월", // 수상 일
+      description:
+        "0과 함께하는 Smarteen App+ Challenge 2019 생활 부문에서 버스커를 위한 버스킹 스트리밍 플랫폼 StreetLife로 최우수상을 수상했습니다. node.js를 사용한 서버개발, Kotlin을 사용한 안드로이드 개발을 맡았습니다.", // 설명
+    },  
+    {
+      title: "서울시 미세먼지 해커톤",
+      host: "서울시", //주최
+      date: "2019년", // 수상 일
+      description:
+        "앱으로 공기 청정 반려식물을 키우는 서비스인 NaMo로 수상하였습니다. Ar과 연동되는 앱 개발을 맡았습니다.", // 설명
+    },
+    {
+      title: "2018 글로벌 디자인 씽킹 해커톤 장관상",
+      host: "환경부", //주최
+      date: "2018년 07월", // 수상 일
+      description:
+        "대전 교육 과학부에서 주최한 글로벌 디자인 씽킹 해커톤에서 Able이라는 장애인 정보 플랫폼 앱을 통해 환경부 장관상을 수상하였습니다. Kotlin을 사용한 안드로이드 개발, Node.js를 사용한 서버개발을 맡았습니다.", // 설명
+    },
+    {
+      title: "Seoul Maker",
+      host: "서울 디지털 제단", //주최
+      date: "2018년", // 수상 일
+      description:
+        "잠수부들을 위한 안전한 고글인 SAFE+GOGGLE로 최우수상을 수상하였습니다. 하드웨어와 Kotlin을 사용한 안드로이드 개발을 맡았습니다.", // 설명
+    },
+    {
+      title: "단국대학교 스타트업 해커톤",
+      host: "단국대학교", //주최
+      date: "2018년", // 수상 일
+      description:
+        "소상공인이 운영하는 카페의 생존을 위한 서비스 ShareCafe로 수상하였습니다. Kotlin을 사용한 안드로이드 개발을 맡았습니다.", // 설명
+    },
+    {
+      title: "모바일 콘텐츠 경진대회",
+      host: "선린인터넷고등학교", //주최
+      date: "2019년", // 수상 일
+      description:
+        "AR로 미리보는 가구 종합 쇼핑몰 서비스인 퍼니퍼니로 수상하였습니다.  Kotlin을 사용한 안드로이드 개발과 node.js를 사용한 서버개발을 맡았습니다.", // 설명
+    },
+    {
+      title: "하이톤 3회 - 4회 연속 최우수상",
+      host: "하이톤", //주최
+      date: "2018년", // 수상 일
+      description:
+        "장애인 스포츠 정보 플랫폼 Spirit In Motion과 약속을 지키게 해주는 블록체인 기반 플랫폼 S: 사람을, M: 만나다, SM프로젝트로 수상하였습니다.  Kotlin을 사용한 안드로이드 개발을 맡았습니다.", // 설명
+    },
+    {
+      title: "Junction X Seoul",
+      host: "Junction", //주최
+      date: "2018년 07월", // 수상 일
+      description:
+        "정션X서울에서 렌파이api를 사용한 Zepeto api 기반 미연시 두근두근 제페토! 서비스로 3rd Prize를 수상하였습니다. node.js를 사용한 서버개발을 하였습니다.", // 설명
+    },
+    {
+      title: "프리즘 창업경진대회",
+      host: "프리즘", //주최
+      date: "2019년 7월", // 수상 일
+      description:
+        "공유경제 기반 물품 대여 서비스 AllOne으로 수상하였습니다.", // 설명
+    },
+    {
+      title: "등의 수상을 하였습니다.",
+      host: "", //주최
+      date: "", // 수상 일
+      description:
+        "", // 설명
+    },
+  ],
+  certificate: [
+
+  ],
+};
